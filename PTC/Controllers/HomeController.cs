325fb9889ea5a263705ff41f3f356fe66a462727
Marco Carcini <marco.carcini@hotmail.com>
2020-07-11 17:20:30 -0500

    Initial commit

---
+++ PTC/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PTCData;
+
+namespace PTC.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      TrainingProductViewModel vm = new TrainingProductViewModel();
+
+      vm.HandleRequest();
+
+      return View(vm);
+    }
+
+    [HttpPost]
+    public ActionResult Index(TrainingProductViewModel vm)
+    {
+      vm.HandleRequest();
+
+      // NOTE: Must clear the model state in order to bind
+      //       the @Html helpers to the new model values
+      ModelState.Clear();
+
+      return View(vm);
+    }
+
+  }
+}
---
+++ PTCData/TrainingProductViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace PTCData
+{
+  public class TrainingProductViewModel
+  {
+    public TrainingProductViewModel()
+    {
+      // Initialize blank list
+      Products = new List<TrainingProduct>();
+      SearchEntity = new TrainingProduct();
+      EventCommand = "List";
+    }
+
+    public List<TrainingProduct> Products { get; set; }
+    public TrainingProduct SearchEntity { get; set; }
+    public string EventCommand { get; set; }
+
+    public void HandleRequest()
+    {
+      switch (EventCommand.ToLower())
+      {
+        case "list":
+        case "search":
+          Get();
+          break;
+
+        case "resetsearch":
+          ResetSearch();
+          Get();
+          break;
+      }
+    }
+
+    private void ResetSearch()
+    {
+      SearchEntity = new TrainingProduct();
+    }
+
+    private void Get()
+    {
+      TrainingProductManager mgr = new TrainingProductManager();
+
+      Products = mgr.Get(SearchEntity);
+    }
+  }
+}
